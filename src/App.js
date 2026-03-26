dd76990cfce99ae931365767cee6140332138195
Shakthiabi06 <shakthiabinaya.v@gmail.com>
2026-03-26 23:14:30 +0530

    modified index.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminLogin from "./components/Admin/AdminLogin";
-import AdminSignup from "./components/Admin/AdminSignup";
-import AdminDashboard from "./pages/AdminDashboard";
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/signup" element={<AdminSignup />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Frontend is working 🚀</h1>
+    </div>
   );
 }
 
